9805c67a2dd4f3f62bcc57eff63d5dc8e74add9f
yatinsingh2007 <yatin4591@gmail.com>
2025-07-27 02:09:28 +0530

    change in frontend url

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use('/uploads' , express.static('uploads'))
 app.use(cors({
-    origin: [process.env.front_end_url, 'http://localhost:5173'],
+    origin: process.env.front_end_url,
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
 }))
