19440a04d2f0ccf3bbb5575596ce63a96d27c068
yatinsingh2007 <yatin4591@gmail.com>
2025-07-26 13:10:57 +0530

    domain added for testing

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use('/uploads' , express.static('uploads'))
 app.use(cors({
-    origin: process.env.front_end_url,
+    origin: [process.env.front_end_url, 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
 }))
